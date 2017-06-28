640b624cf85da672bf43f43824d80b29d363a475
Chris Cantu <pinion31@gmail.com>
2017-06-28 18:26:55 -0500

    code complete

--- server.js
+++ server.js
@@ -10,24 +10,56 @@ http.createServer(function(req,res) {
   var q = url.parse(req.url, true);
 
   var returnStr = "";
-  var query = q.query.request || "";
+  var query = q.query.request || q.pathname.slice(1); //query can be in request header or pathname
 
-  fs.readFile('./static/index.html', function (err, data) {
-    if (err) {return err};
+  checkURLAgainstDB(query, function(err, result) {
+      if (result) { //check if shortened url request exists - if so, reroutes to matching site
+          res.writeHead(302,  {Location: result.original_URL})
+          res.end();
+      }
+      else {
+         //if no matching url request, assumes input value is a website
+         fs.readFile('./static/index.html', function (err, data) { //serves input page
+         if (err) {return err};
 
-    res.writeHead(200, {'Content-type': 'text/html'});
-    res.write(data);
+         res.writeHead(200, {'Content-type': 'text/html'});
+         res.write(data);
 
-    if (query.length > 0) {
-      checkIfValidURL(q.query.request)?  returnStr = putToDB(q.query.request) : returnStr = returnStr.concat("invalid site");
-    }
-
-    res.end("<br>" + returnStr);
-  });
+         //checks if input follows url format. if so, adds to database and assigns shortened url
+         if (query.length > 0) {
+            if (checkIfValidURL(q.query.request)){
+                returnStr = putToDB(q.query.request, function(result) {
+                res.end("<br>" + result.split(',').join("<br>")); //returns input website with shortened url for user
+              })
+            }
+            else {
+              returnStr = returnStr.concat("invalid site"); //input is an invalid website
+              res.end("<br>" + returnStr);
+            }
+          }
+      });
+      }
+})
 }).listen(3000);
 
+function checkURLAgainstDB(index, callback) {
+  mongoClient.connect(urlDB, function(err, db) {
+    if (err) {
+      console.log("Error connecting to database");
+      return;
+    }
+    else {
+            db.collection('urls').findOne({shortened_URL: +index}, function(err,result) {
+                if (err) {console.log('error =' + err);}
+                callback(err,result);
+                db.close();
+            });
+      }
+    }
+  );
+}
 
-function putToDB(entry) {
+function putToDB(entry, callback) {
   var urlEntry = {};
 
   mongoClient.connect(urlDB, function(err, db) {
@@ -36,36 +68,29 @@ function putToDB(entry) {
       return;
     }
     else {
-      console.log(3);
       getNextSequence(db,"userid", function(seqNum) {
           urlEntry = {original_URL: entry, shortened_URL:seqNum};
           db.collection('urls').insertOne(urlEntry);
           db.close();
-          return JSON.stringify(urlEntry);
+          callback(JSON.stringify(urlEntry));
       });
     }
-
-
   });
 }
 
 function getNextSequence(db, name, callback) {
   db.collection('counters').findAndModify(
-
            { _id: name},
            {},
            { $inc: { seq: 1 } },
            {new:true}, {upsert:true},
            function(err,result) {
-            console.log(result.value.seq);
             callback(result.value.seq);
            }
-
    );
 }
 
 function checkIfValidURL(site){
-
   var valid = false;
   var shortUrl = /^http[s]?:\/\/[a-zA-Z0-9]+[\.]{1}[a-zA-Z0-9]{3}$/;
   var longUrl = /^http[s]?:\/\/[www]{3}[\.]{1}[a-zA-Z0-9]+[\.]{1}[a-zA-Z0-9]{3}$/;
@@ -73,18 +98,6 @@ function checkIfValidURL(site){
   if (site.search(shortUrl) !== -1 || site.search(longUrl) !== -1) {
     return true;
   }
-
   return false;
 }
 
-//this increments correctly
-/*
- db.collection('counters').findAndModify(
-
-           { _id: name},
-           {},
-           { $inc: { seq: 1 } },
-           {new:true, upsert:true}
-        // callback()
-   );
-*/
